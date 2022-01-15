1dca5e93ab2d1dcc92b8c25ef7586058346d6223
kachark <kkachar@hotmail.com>
2022-01-15 12:47:08 -0500

    added config for wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+
+local wezterm = require 'wezterm';
+return {
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  font_size = 14.5;
+  color_scheme = "Snazzy",
+  hide_tab_bar_if_only_one_tab = true,
+}
