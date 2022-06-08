4ded5fabf9c26e08314fefe9ea9a658e86d447c3
kachark <kkachar@hotmail.com>
2022-06-07 22:00:02 -0500

    added new configs for nvim plugins

---
+++ .config/nvim/lua/config/treesitter.lua
@@ -0,0 +1,24 @@
+
+--- TreeSitter ---
+
+local treesitter = require 'nvim-treesitter.configs'
+treesitter.setup {
+  ensure_installed = { "c", "cpp", "lua", "rust", "python" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  -- ignore_install = { "javascript" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    disable = { },  -- list of language that will be disabled
+  },
+}
+
+treesitter.setup {
+  highlight = {
+    enable = true,
+    custom_captures = {
+      -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+      ["foo.bar"] = "Identifier",
+    },
+  },
+}
+
+
