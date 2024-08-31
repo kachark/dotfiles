67c3cedd59deab8f581526f5ca7c9a972dc8ed49
kachark <kkachar@hotmail.com>
2024-08-31 09:43:36 -0400

    updated whichkey config

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -14,81 +14,67 @@ function M.setup()
     nowait = false, -- use `nowait` when creating keymaps
   }
 
-  local presets = require("which-key.plugins.presets")
-
   local mappings = {
-    ["<leader>"] = {
-      ["<leader>"] = {
-        name = "+Quick Commands",
-        c = { "<cmd>noh<cr>", "Clear Highlights" },
-        d = { "<cmd>bd<cr>", "Close Current Buffer" },
-        y = { '"+y', "Yank To + Register" },
-      },
+    { "<leader><leader>", group = "+Quick Commands" },
+    { "<leader><leader>c", "<cmd>noh<cr>", desc = "Clear Highlights"},
+    { "<leader><leader>d", "<cmd>bd<cr>", desc = "Close Current Buffer"},
+    { "<leader><leader>y", "+y", desc = "Yank To + Register"},
 
-      f = {
-        name = "+Find",
-        -- FZF
-        f = { "<cmd>FzfLua files<cr>", "Find File" },
-        g = { "<cmd>FzfLua git_files<cr>", "Find Git Files" },
-        b = { "<cmd>FzfLua buffers<cr>", "Find Buffers" },
-        h = { "<cmd>FzfLua btags<cr>", "Find Buffer Tags" },
-        -- a = { "<cmd>FzfLua grep_project<cr>", "Grep Project" },
-        a = { "<cmd>FzfLua live_grep_glob<cr>", "Grep Project" },
-        s = { "<cmd>FzfLua lgrep_curbuf<cr>", "Grep Buffer" },
+    { "<leader>f", group = "+Find" },
+    -- -- FZF
+    { "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find File" },
+    { "<leader>fg", "<cmd>FzfLua git_files<cr>", desc = "Find Git Files" },
+    { "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "Find Buffers" },
+    { "<leader>fh", "<cmd>FzfLua btags<cr>", desc = "Find Buffer Tags" },
+    -- { "<leader>fa", "<cmd>FzfLua grep_project<cr>", desc = "Grep Project" },
+    { "<leader>fa", "<cmd>FzfLua live_grep_glob<cr>", desc = "Grep Project" },
+    { "<leader>fs", "<cmd>FzfLua lgrep_curbuf<cr>", desc = "Grep Buffer" },
 
-        -- Telescope
-        -- f = { "<cmd>Telescope find_files<cr>", "Find File" },
-        -- g = { "<cmd>Telescope git_files<cr>", "Find Git Files" },
-        -- b = { "<cmd>Telescope buffers<cr>", "Find Buffers" },
-        -- h = { "<cmd>Telescope current_buffer_tags<cr>", "Find Buffer Tags" },
-        -- a = { "<cmd>Telescope live_grep<cr>", "Grep Project" },
-        -- s = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Grep Buffer" },
-      },
+    -- -- Telescope
+    -- { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File" },
+    -- { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Git Files" },
+    -- { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
+    -- { "<leader>fh", "<cmd>Telescope current_buffer_tags<cr>", desc = "Find Buffer Tags" },
+    -- { "<leader>fa", "<cmd>Telescope live_grep<cr>", desc = "Grep Project" },
+    -- { "<leader>fs", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Grep Buffer" },
 
-      l = {
-        name = "+Language Server",
-        -- Trouble
-        r = { "<cmd>Trouble lsp toggle focus=false win.position=right win.size.width=0.4<cr>", "Show Symbol References" },
-        y = { "<cmd>Trouble symbols toggle focus=false win.size.width=0.4<cr>", "Show Symbol Tree For Current Buffer" },
+    { "<leader>l", group = "+Language Server" },
+    -- Trouble
+    { "<leader>lr", "<cmd>Trouble lsp toggle focus=false win.position=right win.size.width=0.4<cr>", desc = "Show Symbol References" },
+    { "<leader>ly", "<cmd>Trouble symbols toggle focus=false win.size.width=0.4<cr>", desc = "Show Symbol Tree For Current Buffer" },
+    -- NVIM-LSP
+    { "<leader>ld", "<cmd>lua vim.lsp.buf.definition()<cr>", desc = "Jump To Symbol Definition" },
+    { "<leader>ln", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "Rename All References To Symbol" },
+    { "<leader>ls", "<cmd>lua vim.lsp.buf.hover()<cr>", desc = "Display Symbol Information. Use Twice To Jump Into Window" },
+    { "<leader>li", "<cmd>lua vim.lsp.buf.implementation()<cr>", desc = "List All Symbol Implementations" },
+    { "<leader>lh", "<cmd>lua vim.lsp.buf.signature_help()<cr>", desc = "Display Symbol Signature Information" },
+    { "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Prompt For Code Actions" },
 
-        -- NVIM-LSP
-        d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Jump To Symbol Definition" },
-        n = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename All References To Symbol" },
-        s = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Display Symbol Information. Use Twice To Jump Into Window" },
-        i = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "List All Symbol Implementations" },
-        h = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Display Symbol Signature Information" },
-        a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Prompt For Code Actions" },
-      },
+    { "<leader>q", group = "+Diagnostics" },
+    { "<leader>q[", "<cmd>lua vim.diagnostic.goto_prev()<cr>", desc = "Jump To Prev Diagnostic" },
+    { "<leader>q]", "<cmd>lua vim.diagnostic.goto_next()<cr>", desc = "Jump To Next Diagnostic" },
+    { "<leader>qq", "<cmd>lua vim.diagnostic.open_float(0, {scope='cursor'})<cr>", desc = "Show Diagnostics Under Cursor" },
+    { "<leader>qQ", "<cmd>lua vim.diagnostic.open_float(0, {scope='line'})<cr>", desc = "Show Diagnostics For Line" },
+    -- -- Trouble
+    { "<leader>qd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Document Diagnostics" },
+    { "<leader>qD", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
+    { "<leader>ql", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix list" },
+    { "<leader>qt", "<cmd>Trouble todo toggle<cr>", desc = "Todo" },
+    { "<leader>qT", "<cmd>Trouble todo toggle filter={tag = {TODO, FIX, FIXME}}<cr>", desc = "Todo/Fix/Fixme" },
 
-      q = {
-        name = "+Diagnostics",
-        ["["] = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Jump To Prev Diagnostic" },
-        ["]"] = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Jump To Next Diagnostic" },
-        q = { "<cmd>lua vim.diagnostic.open_float(0, {scope='cursor'})<cr>", "Show Diagnostics Under Cursor" },
-        Q = { "<cmd>lua vim.diagnostic.open_float(0, {scope='line'})<cr>", "Show Diagnostics For Line" },
-
-        -- Trouble
-        d = { "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", "Document Diagnostics" },
-        D = { "<cmd>Trouble diagnostics toggle<cr>", "Diagnostics" },
-      },
-
-      x = {
-        name = "+Debug (Must Be Configured)",
-        -- NVIM-DAP (Debugger)
-        b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
-        c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
-        o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
-        i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
-        O = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
-        r = { "<cmd>lua require'dap'.repl.open()<cr>", "Open REPL" },
-        l = { "<cmd>lua require'dap'.run_last()<cr>", "Run Last" },
-        u = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle UI" },
-      }
-
-    },
+    { "<leader>x", group = "+Debug (Must Be Configured)" },
+    -- NVIM-DAP (Debugger)
+    { "<leader>b", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", desc = "Toggle Breakpoint" },
+    { "<leader>c", "<cmd>lua require'dap'.continue()<cr>", desc = "Continue" },
+    { "<leader>o", "<cmd>lua require'dap'.step_over()<cr>", desc = "Step Over" },
+    { "<leader>i", "<cmd>lua require'dap'.step_into()<cr>", desc = "Step Into" },
+    { "<leader>O", "<cmd>lua require'dap'.step_out()<cr>", desc = "Step Out" },
+    { "<leader>r", "<cmd>lua require'dap'.repl.open()<cr>", desc = "Open REPL" },
+    { "<leader>l", "<cmd>lua require'dap'.run_last()<cr>", desc = "Run Last" },
+    { "<leader>u", "<cmd>lua require'dapui'.toggle()<cr>", desc = "Toggle UI" },
   }
 
-  wk.register(mappings, opts)
+  wk.add(mappings, opts)
 
 end
 
